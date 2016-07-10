8f85728a8f60e0c2f184f55d75990628c9fe7867
Rogelio Cedillo <rogelio.cedillo@sap.com>
2016-07-10 08:50:06 -0500

    m-cli is updated if it is already installed

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ PKG="m-cli"
 GIT_URL="https://github.com/rgcr/m-cli.git"
 INSTALL_DIR="/usr/local"
 
+_WAS="Installed"
+
 
 has(){
     type "$1" > /dev/null 2>&1
@@ -11,41 +13,52 @@ has(){
 
 
 install_from_git(){
-    [ -e "${INSTALL_DIR}/${PKG}" ] && rm -rf "${INSTALL_DIR}/${PKG}" 2>/dev/null
-    echo "Downloading ${PKG} from git to ${INSTALL_DIR}"
-    command git clone --depth 1 ${GIT_URL} ${INSTALL_DIR}/${PKG} || {
-        echo >&2 "Failed to clone."
-        exit 1
-    }
-    chmod -R 755 ${INSTALL_DIR}/${PKG}/lib  2>/dev/null
-    chmod -R 755 ${INSTALL_DIR}/${PKG}/plugins 2>/dev/null
-    chmod -R 755 ${INSTALL_DIR}/${PKG}/m 2>/dev/null
+    if [ -e "${INSTALL_DIR}/${PKG}" ]; then
+        echo "${PKG} Is already installed"
+        echo "Updating ${PKG} from git"
+        command git --git-dir="${INSTALL_DIR}/${PKG}/.git" --work-tree="${INSTALL_DIR}/${PKG}" fetch --all || {
+            echo >&2 "Failed to fetch changes => ${GIT_URL}"
+            exit 1
+        }
+        command git --git-dir="${INSTALL_DIR}/${PKG}/.git" --work-tree="${INSTALL_DIR}/${PKG}" reset --hard origin/master || {
+            echo >&2 "Failed to reset changes => ${GIT_URL}"
+            exit 1
+        }
+        _WAS="Updated"
+    else
+        echo "Downloading ${PKG} from git to ${INSTALL_DIR}"
+        command git clone --depth 1 ${GIT_URL} ${INSTALL_DIR}/${PKG} || {
+            echo >&2 "Failed to clone => ${GIT_URL}"
+            exit 1
+        }
+        chmod -R 755 ${INSTALL_DIR}/${PKG}/lib  2>/dev/null
+        chmod -R 755 ${INSTALL_DIR}/${PKG}/plugins 2>/dev/null
+        chmod 755 ${INSTALL_DIR}/${PKG}/m 2>/dev/null
+        _WAS="Installed"
+    fi
 }
 
-#TODO: ADD CURL INSTALLATION METHOD
-#install_from_curl(){
-#}
 
 if has "git"; then
     install_from_git;
-#elif has "curl"; then
-    #install_from_curl
 else
     echo >&2 "Failed to install, please install git before."
 fi
 
 if [ -f "${INSTALL_DIR}/${PKG}/m" ]; then
-    cat<<__EOF__
+    if [ "${_WAS}" == "Installed" ]; then
+        cat<<__EOF__
 
 
-Installed successfully!
+    Installed successfully!
 
-Please add the following line to your .bashrc or .zshrc or .profile:
+    Please add the following line to your .bashrc or .zshrc or .profile:
 
-export PATH=\$PATH:${INSTALL_DIR}/${PKG}
+    export PATH=\$PATH:${INSTALL_DIR}/${PKG}
 
 
 __EOF__
+    fi
 else
     echo >&2 ""
     echo >&2 "Something went wrong. ${INSTALL_DIR}/${PKG}/m not found"
